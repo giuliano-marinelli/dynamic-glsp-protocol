f531b289992c31abdc8734bca292660f83a0d7cc
Giuliano Marinelli <giuliano.marinelli@hotmail.com>
2025-02-18 14:31:46 -0300

    added style and other specific type properties to amodel.

--- src/amodel.ts
+++ src/amodel.ts
@@ -1,14 +1,29 @@
 export enum Type {
-  STRING = "string",
-  INTEGER = "integer",
-  BOOLEAN = "boolean",
-  ARRAY = "array",
-  OBJECT = "object",
+  STRING = 'string',
+  INTEGER = 'integer',
+  BOOLEAN = 'boolean',
+  ARRAY = 'array',
+  OBJECT = 'object'
+}
+
+export enum StringStyle {
+  INPUT = 'input',
+  TEXTAREA = 'textarea'
+}
+
+export enum IntegerStyle {
+  INPUT = 'input',
+  RANGE = 'range'
+}
+
+export enum BooleanStyle {
+  CHECKBOX = 'checkbox',
+  SWITCH = 'switch'
 }
 
 export enum EnumStyle {
-  SELECT = "select",
-  RADIO = "radio",
+  SELECT = 'select',
+  RADIO = 'radio'
 }
 
 export interface AModelElementSchema {
@@ -18,16 +33,31 @@ export interface AModelElementSchema {
   default?: any;
   readOnly?: boolean;
   rule?: any;
-  asd?: any;
+}
+
+export interface AModelStringSchema extends AModelElementSchema {
+  type: Type.STRING;
+  minLength?: number;
+  maxLength?: number;
+  style?: StringStyle;
+}
+
+export interface AModelIntegerSchema extends AModelElementSchema {
+  type: Type.INTEGER;
+  minimum?: number;
+  maximum?: number;
+  style?: IntegerStyle;
+}
+
+export interface AModelBooleanSchema extends AModelElementSchema {
+  type: Type.BOOLEAN;
+  style?: BooleanStyle;
 }
 
 export interface AModelEnumSchema extends AModelElementSchema {
   type: Type.STRING | Type.INTEGER | Type.BOOLEAN;
-  enum?:
-    | [{ const: string | number | boolean; title: string }]
-    | string[]
-    | number[]
-    | boolean[];
+  enum?: [{ const: string | number | boolean; title: string }] | string[] | number[] | boolean[];
+  style?: EnumStyle;
 }
 
 export interface AModelArraySchema extends AModelElementSchema {
